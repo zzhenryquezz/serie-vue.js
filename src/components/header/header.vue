5c3e8dbbf1c7a5d0f4f830f560156a0ed4168a81
henrique-manjaro <henriqueoliwork@gmail.com>
2019-06-21 18:10:19 -0300

    Ep complete

---
+++ src/components/header/header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header class="header">
+        <div class="entry-content container-fluid">
+
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <img src="./../../assets/logo.png" alt="">
+                </div>
+                <div class="col-12 col-md-8">
+                    <h1>Blog Vue</h1>
+                </div>
+            </div>
+        </div>
+        <div class="entry-navbar">
+
+            <ul class="nav justify-content-center pt-2 pb-2 bg-dark">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="container">
+        <h1>Artigos</h1>
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style>
+main{
+    padding-top: 40px;
+    min-height: 80vh;
+}
+</style>
